072af60503c0c9d99deb45f144232334e596c8f9
Eddie Liu <edwl@microsoft.com>
2022-03-07 09:55:44 -0800

    blh blah

--- src/pages/work/careercafe/careercafe.tsx
+++ src/pages/work/careercafe/careercafe.tsx
@@ -471,9 +471,6 @@ export const CareerCafe = () => {
             <div className={appStyles.flexibleContainer}>
               <iframe
                 title={"Prototype"}
-                style={{
-                  border: "1px solid rgba(0, 0, 0, 0.1)",
-                }}
                 width="800"
                 height="450"
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FGohLRf2xv3xHkl9K1tJ85D%2Fcareercaf%25C3%25A9%3Fpage-id%3D0%253A1%26node-id%3D2%253A2%26viewport%3D241%252C48%252C0.14%26scaling%3Dscale-down%26starting-point-node-id%3D2%253A2"
