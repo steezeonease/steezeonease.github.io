9c50ef28fd25f96f29257c84717e91e65af5edd3
Eddie Liu <edwl@microsoft.com>
2022-03-08 11:08:58 -0800

    sticky nav

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -2,24 +2,37 @@ import { css, IconButton } from "@fluentui/react";
 import React from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./NavigationBar.module.scss";
-import appStyles from "../../css/App.module.scss";
 import { NavigationLink } from "./NavigationLink";
 
 export const NavigationBar: React.FC = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [hasShadow, setHasShadow] = React.useState<boolean>(false);
 
   const onToggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   const location = useLocation();
-  console.log(location);
+
+  const onDocumentScroll = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    setHasShadow(c > 20);
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("scroll", onDocumentScroll);
+
+    return function cleanUp() {
+      document.removeEventListener("scroll", onDocumentScroll);
+    };
+  });
 
   return (
-    <div className={appStyles.containerSection}>
+    <div className={styles.root}>
       <div
         className={css(styles.navigationContainer, {
           [styles.isOpen]: isOpen,
+          [styles.hasShadow]: hasShadow,
         })}
       >
         <div className={styles.menuButton}>
@@ -39,27 +52,29 @@ export const NavigationBar: React.FC = () => {
             />
           )}
         </div>
-        <nav>
-          <NavigationLink
-            location={location}
-            text="Work"
-            toPath="/work"
-            onToggleMenu={onToggleMenu}
-          />
-          <NavigationLink
-            location={location}
-            text="About"
-            toPath="/about"
-            onToggleMenu={onToggleMenu}
-          />
-          <a
-            href="https://dl.dropboxusercontent.com/s/bhrbenkkdwn50em/Hannah%20Yi%20Resume.pdf"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Resume
-          </a>
-        </nav>
+        <div className={styles.navParent}>
+          <nav>
+            <NavigationLink
+              location={location}
+              text="Work"
+              toPath="/work"
+              onToggleMenu={onToggleMenu}
+            />
+            <NavigationLink
+              location={location}
+              text="About"
+              toPath="/about"
+              onToggleMenu={onToggleMenu}
+            />
+            <a
+              href="https://dl.dropboxusercontent.com/s/bhrbenkkdwn50em/Hannah%20Yi%20Resume.pdf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume
+            </a>
+          </nav>
+        </div>
       </div>
     </div>
   );
